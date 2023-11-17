01f149cbf2b0bbf2b29ad94705510e81580179af
Ludi Rehak <ludi317@gmail.com>
2023-11-16 18:35:54 -0800

    Day 19 was a killer. I had to look at the subreddit for the solution: Iterate through the replacements and replace everything you can, whittling down the target molecule to a smaller molecule, until it becomes a precursor to an 'e'. If it can't be reduced before then, shuffle the replacement and try again.

--- src/2015/lib.rs
+++ src/2015/lib.rs
@@ -1,61 +1,72 @@
 // https://adventofcode.com/2015/
 
-use std::mem;
-
-pub fn day_18() {
-    let mut grid = [[0u8; 100]; 100];
-
+use std::collections::{HashMap, HashSet};
+use rand::seq::SliceRandom; // Import the necessary trait
+use rand::thread_rng;
+pub fn day_19() {
     let input = include_str!("input/raw.txt");
-    for (i, line) in input.lines().enumerate() {
-        for (j, char) in line.chars().enumerate() {
-            if char == '#' {
-                grid[i][j] = 1;
+    let lines = input.lines();
+    let mut replace_list: Vec<(&str, &str)> = Vec::new();
+    let mut parse_molecule = false;
+    let mut to_e: HashSet<String> = HashSet::new();
+    let mut orig_target = "".to_string();
+    for line in lines {
+        if line == "" {
+            parse_molecule = true;
+            continue;
+        }
+        if parse_molecule {
+            orig_target = line.to_string();
+        } else {
+            let parts: Vec<&str> = line.split(" => ").collect();
+            if parts[0] == "e" {
+                to_e.insert(parts[1].to_string());
+                continue;
             }
+            replace_list.push((parts[1], parts[0]));
         }
     }
 
-    let mut out = [[0u8; 100]; 100];
-    for _ in 0..100 {
-        one_step(grid, &mut out);
-        mem::swap(&mut grid, &mut out);
+    let mut count = 0;
+    let mut partial_count = 0;
+    let mut target:String = orig_target.clone();
+
+    'outerLoop: while !to_e.contains(&target) {
+        let prev_target = target.clone();
+        (target, partial_count) = many_steps(&replace_list, target);
+        if prev_target == target {
+            // no progress, try again
+            println!("failed to reduce target to nothing, reshuffling replacement order");
+            target = orig_target.clone();
+            count = 0;
+            let mut rng = thread_rng();
+            replace_list.shuffle(&mut rng);
+            continue 'outerLoop;
+        }
+        count += partial_count;
+        println!("{target} {count}");
     }
 
-    let mut sum: u16 = 0;
-    for row in grid.iter() {
-        for &value in row.iter() {
-            sum += value as u16;
-        }
-    }
-    println!("{}", sum);
+    println!("{}", count + 1);
 }
 
-fn one_step(grid: [[u8; 100]; 100], out: &mut [[u8; 100]; 100]) {
-    for i in 0..grid.len() {
-        for j in 0..grid[0].len() {
-            let mut num_neighs = 0;
-            let row_min = 0.max(i as i8 - 1) as usize;
-            for row in row_min..=(grid.len() - 1).min(i + 1) {
-                let col_min = 0.max(j as i8 - 1) as usize;
-                for col in col_min..=(grid[0].len() - 1).min(j + 1) {
-                    if i == row && col == j {
-                        continue;
-                    }
-                    if grid[row][col] == 1 {
-                        num_neighs += 1;
-                    }
-                }
-            }
-            out[i][j] = grid[i][j];
-            if grid[i][j] == 1 && !(num_neighs == 2 || num_neighs == 3) {
-                out[i][j] = 0;
-            } else if grid[i][j] == 0 && num_neighs == 3 {
-                out[i][j] = 1;
-            }
+fn many_steps(replace_list: &Vec<(&str, &str)>, mut mol: String) -> (String, usize) {
+    let mut count = 0;
+    for (k, v) in replace_list {
+        count += mol.match_indices(k).count();
+        mol = mol.replace(k, v);
+    }
+    (mol, count)
+}
+
+fn one_step(replace_map: &HashMap<&str, &str>, mol: &str) -> Vec<String> {
+    let mut replaced: Vec<String> = vec![];
+    for (k, v) in replace_map {
+        let vec: Vec<_> = mol.match_indices(k).collect();
+        for (ind, _) in vec {
+            let new_mol = format!("{}{}{}", &mol[..ind], v, &mol[(ind + (k.len()))..]);
+            replaced.push(new_mol);
         }
     }
-    out[0][0] = 1;
-    out[0][99] = 1;
-    out[99][0] = 1;
-    out[99][99] = 1;
-
+    replaced
 }
